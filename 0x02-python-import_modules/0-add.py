b8bc6e89d583fdc88299b7f431cb9f5dc63a9bac
Dammynova7 <nwalagad2000@gmail.co>
2023-08-12 00:18:12 +0100

    Fixed solution for Task #0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 
-# Import a user defined module
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-# variable assignments
 a = 1
 b = 2
-result = add(a, b)
 
-# prints the result using string format
-print("{} + {} = {}".format(a, b, result))
-
-# prevents code from executing when imported in another py program
-if __name__ == "__main__":
-    import sys
-    sys.argv[0]
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
