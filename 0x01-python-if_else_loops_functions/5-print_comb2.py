f7da43d2b6b4d9084c9617745de42dcac7002d5f
Dammynova7 <nwalagad2000@gmail.co>
2024-01-03 01:09:11 +0100

    Fix: 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 
 for i in range(0, 100):
     if i < 99:
-        print("{:d}".format(i), end=", ")
+        print("{:d}{:d}".format(i // 10, i % 10), end=", ")
 
     if i == 99:
         print("{:d}".format(i), end="\n")
