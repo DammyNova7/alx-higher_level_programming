30de7683f6f6d7bb0a6c00ca441018b7b777f721
Dammynova7 <nwalagad2000@gmail.co>
2024-01-09 13:59:59 +0100

    Fixed script for cases where by the index passed to the function is outside the index of the list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
+    n = len(my_list)
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx > n:
         return None
     else:
-        for i in my_list:
+        for i in range(n + 1):
             if i == idx:
                 return my_list[i]
