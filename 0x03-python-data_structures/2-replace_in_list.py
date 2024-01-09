ec413588ef2f8946e8a3154f82451dcaa134b0c5
Dammynova7 <nwalagad2000@gmail.co>
2024-01-09 14:51:53 +0100

    Fixed the edge cases for when the idx is less than index 0 or greater than the length of the list. Previously used a print statement instead of return

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,9 +3,9 @@
 def replace_in_list(my_list, idx, element):
     size = len(my_list)
     if idx < 0:
-        print(my_list)
+        return (my_list)
     elif idx >= size:
-        print(my_list)
+        return (my_list)
     else:
         for i in range(size + 1):
             if i == idx:
