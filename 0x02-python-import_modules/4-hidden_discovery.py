4ee549b9df05501163a7804bd23ba3a242f80662
DammyNova7 <nwalagaddammy7@gmail.com>
2024-03-05 01:54:38 -0800

    Fix: 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import hidden_4.pyc
-    names = dir(hidden_4.pyc)
+    import hidden_4
+    names = dir(hidden_4)
 
     exceptions = [i for i in names if not i.startswith('__')]
     for i in exceptions:
