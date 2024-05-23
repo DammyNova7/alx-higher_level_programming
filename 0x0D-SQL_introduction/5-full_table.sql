197a87a98f395a5fe19b251ed944c55cd9b214c8
DammyNova7 <nwalagaddammy7@gmail.com>
2024-05-24 00:17:09 +0100

    Add: 5-full_table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- prints the full description of a the table first_table.
-SELECT *
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
 
