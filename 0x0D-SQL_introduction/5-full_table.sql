8576a2107f9532af75b6f0d9d8454c2d4eaeaea7
DammyNova7 <nwalagaddammy7@gmail.com>
2024-05-23 08:40:40 +0100

    Hot Fix: 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- prints the full description of a the table first_table.
-SELECT * FROM COLUMNS WHERE hbtn_0c_0 AND first_table;
+SELECT * FROM information_schema.COLUMNS WHERE hbtn_0c_0 AND first_table;
 
