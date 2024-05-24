ec13b1db01ad109c728fa1c98751130ba7a17913
DammyNova7 <nwalagaddammy7@gmail.com>
2024-05-24 02:01:00 +0100

    Add: 15-groups

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- ists the number of records with the same score in the table second_table of the database.
-SELECT score, COUNT(*) AS num
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
-ORDER BY num DESC;
+ORDER BY number DESC;
 
