4c56d4a5e88f4a98b749ef8e08c72d24d9d1cc25
DammyNova7 <nwalagaddammy7@gmail.com>
2024-06-11 11:11:55 +0100

    Add: 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# function removes all 'c' and 'C' chars from a string
+
+def no_c(my_string):
+    new_string = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_string += i
+    return (new_string)
